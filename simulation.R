bc8739847616f2c33d91eef00f5dbcc625de1220
varsha101096 <varsha.ramakrishnan@ucdconnect.ie>
2025-08-06 15:07:54 +0100

    Test simulation

---
+++ simulation.R
@@ -0,0 +1,6 @@
+# Define 3 scenarios with vehicle counts and duration
+scenarios <- list(
+  A = list(duration = 7, trucks = 20, tanks = 5, aircraft = 0),
+  B = list(duration = 30, trucks = 100, tanks = 20, aircraft = 5),
+  C = list(duration = 90, trucks = 500, tanks = 200, aircraft = 30)
+)
